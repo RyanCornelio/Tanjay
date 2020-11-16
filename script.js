ffa866c1a82e0174ed2d25be0ea585c4660dfe06
Ryan-Phillips Cornelio <ryan.cornelio@gmail.com>
2020-11-16 15:46:08 +0100

    init
    added

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("main").innerHTML=deltagare;
+
+let shouldGetIntoIf = true
+
+let randomNumber = Math.floor(Math, random() * 10)
+
+if(randomNumber ==2) {
+    alert(randomNumber)
+} else if(randomNumber== 3) {
+    console.log("randomNumber är inte 2")
+}
